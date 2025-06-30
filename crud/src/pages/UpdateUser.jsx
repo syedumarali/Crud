3ea2d19b56e35a01ee37e463b4fdcb14ccb50b25
syedumarali <umaxkhan786@gmail.com>
2025-06-30 16:32:05 +0500

    changes

--- crud/src/pages/UpdateUser.jsx
+++ crud/src/pages/UpdateUser.jsx
@@ -36,9 +36,6 @@ const UpdateUser = () => {
     }
 
 
-
-
-
   return (
        <div className='d-flex vh-100 bg-primary justify-content-center align-items-center '>
       <div className="w-50 bg-white rounded p-3">
